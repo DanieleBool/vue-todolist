2a9540755b2f6aabecde389428281d1f6eb3d2e1
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2021-11-29 23:59:19 +0100

    esercizio con errori

---
+++ script.js
@@ -0,0 +1,60 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        todosChristmas: [
+            {
+                task: "Non avrai altro Dio all'infuori di me",
+                done: true,
+            },
+            {
+                task: "Non nominare il nome di Dio invano.",
+                done: true,
+            },
+            {
+                task: "Ricordati di santificare le feste.",
+                done: true,
+            },
+            {
+                task: "Onora il padre e la madre.",
+                done: false,
+            },
+            {
+                task: "Non uccidere.",
+                done: false,
+            },
+            {
+                task: "Non commettere atti impuri",
+                done: false,
+            },
+        ],
+
+        inputValue: '',
+
+    },
+
+    methods: {
+        addTodo: function(){
+            if(this.inputValue != "" ){
+                this.todosChristmas.push(this.inputValue);
+                this.inputValue = "";
+            }else if(this.inputValue = " "){
+                alert('no')
+            }else{
+
+            }
+        },
+
+        removeTodo: function(index){
+            this.todosChristmas.splice(index, 1);
+        },
+
+        doneJs: function(){
+            if(this.todosChristmas.done == true){
+                return false;
+                
+            } else {
+                return true;
+            }
+        }
+    },
+});
